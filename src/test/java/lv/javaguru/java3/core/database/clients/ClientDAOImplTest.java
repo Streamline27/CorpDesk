2bc610786a5f1baef878fb6fe8aa08f4f85ec904
techik <klad@inbox.lv>
2015-10-31 20:39:22 +0200

    Add some test data to db and init HSQL db manager

--- src/test/java/lv/javaguru/java3/core/database/clients/ClientDAOImplTest.java
+++ src/test/java/lv/javaguru/java3/core/database/clients/ClientDAOImplTest.java
@@ -1,24 +1,39 @@
 package lv.javaguru.java3.core.database.clients;
 
+import lv.javaguru.java3.config.AppCoreConfig;
 import lv.javaguru.java3.core.domain.Client;
 import org.junit.Test;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import static lv.javaguru.java3.core.domain.ClientBuilder.createClient;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
+
 public class ClientDAOImplTest extends DatabaseHibernateTest {
+
+    private final boolean SH = false;
 
     @Test
     @Transactional
     public void testCreateClient() {
+
+
         Client client = createClient()
                 .withLogin("login")
                 .withPassword("password").build();
         assertThat(client.getId(), is(nullValue()));
         clientDAO.create(client);
         assertThat(client.getId(), is(notNullValue()));
+
+        if (SH) {
+            useDBManager();
+        }
+
+
     }
 
     @Test
@@ -30,6 +45,19 @@ public class ClientDAOImplTest extends DatabaseHibernateTest {
         clientDAO.create(client);
         Client clientFromDb = clientDAO.getById(client.getId());
         assertThat(clientFromDb, is(notNullValue()));
+
+
+    }
+
+    private void useDBManager(){
+        try {
+            org.hsqldb.util.DatabaseManagerSwing.main(new String[]{
+                    "--url", "jdbc:hsqldb:mem:testdb", "--noexit"
+                });
+                Thread.currentThread().join();
+        }catch (Exception e){
+            System.out.println("Thread Exception");
+        }
     }
 
 }
