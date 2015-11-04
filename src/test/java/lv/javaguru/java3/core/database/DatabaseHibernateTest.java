cb53243111519cc6e40b093f59265ffcf4520a0c
jasmine7 <lanai@inbox.lv>
2015-11-04 17:21:21 +0200

    Some tests for PostDAO and CommentDAO.

--- src/test/java/lv/javaguru/java3/core/database/DatabaseHibernateTest.java
+++ src/test/java/lv/javaguru/java3/core/database/DatabaseHibernateTest.java
@@ -2,6 +2,8 @@ package lv.javaguru.java3.core.database;
 
 import lv.javaguru.java3.config.AppCoreConfig;
 import lv.javaguru.java3.core.database.client.ClientDAO;
+import lv.javaguru.java3.core.database.posts.CommentDAO;
+import lv.javaguru.java3.core.database.posts.PostDAO;
 import lv.javaguru.java3.core.database.user.GroupDAO;
 import lv.javaguru.java3.core.database.user.UserDAO;
 import org.hibernate.SessionFactory;
@@ -25,4 +27,10 @@ public abstract class DatabaseHibernateTest {
 
 	@Autowired
 	protected GroupDAO groupDAO;
+
+    @Autowired
+    protected PostDAO postDAO;
+
+    @Autowired
+    protected CommentDAO commentDAO;
 }
